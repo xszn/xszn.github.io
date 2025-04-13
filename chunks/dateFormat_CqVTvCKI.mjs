41d45cc30ec136a4cf69b97afa072b37b763605c
clashaka <clashaka@outlook.com>
2025-04-13 09:12:54 +0800

    Add new content

--- chunks/dateFormat_CqVTvCKI.mjs
+++
@@ -1,9 +0,0 @@
-import { format } from 'date-fns';
-
-const dateFormat = (date, pattern = "yyyy-MM-dd") => {
-  const dateObj = new Date(date);
-  const output = format(dateObj, pattern);
-  return output;
-};
-
-export { dateFormat as d };
--- chunks/taxonomyFilter_CE1MAD_3.mjs
+++
@@ -1,7 +0,0 @@
-import { s as slugify } from './_astro_content_DxdUGNXu.mjs';
-
-const taxonomyFilter = (posts, name, key) => posts.filter(
-  (post) => post.data[name].map((name2) => slugify(name2)).includes(key)
-);
-
-export { taxonomyFilter as t };
