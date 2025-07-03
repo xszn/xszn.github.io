31c108344692fd8b248d1fd7d5122b5834d22af4
clashaka <clashaka@outlook.com>
2025-07-03 10:01:35 +0800

    Add new content

--- chunks/sortFunctions_DN17Ikpp.mjs
+++
@@ -1,8 +0,0 @@
-const sortByDate = (array) => {
-  const sortedArray = array.sort(
-    (a, b) => new Date(b.data.date && b.data.date).valueOf() - new Date(a.data.date && a.data.date).valueOf()
-  );
-  return sortedArray;
-};
-
-export { sortByDate as s };
